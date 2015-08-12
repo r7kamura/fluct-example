44d92e2e4a1b795576572b3240afdcd081adc7d8
Ryo Nakamura <r7kamura@gmail.com>
2015-08-13 01:18:13 +0900

    Fix README path using symlink

--- actions/show_readme/index.js
+++ actions/show_readme/index.js
@@ -2,7 +2,7 @@ var marked = require('marked');
 var fs = require('fs');
 
 exports.handler = function (event, context) {
-  var markdownFilePath = __dirname + '/../../README.md';
+  var markdownFilePath = __dirname + '/README.md';
   var markdownString = fs.readFileSync(markdownFilePath, { encoding: 'utf8' });
   var responseBody = marked(markdownString);
   context.succeed(responseBody);
