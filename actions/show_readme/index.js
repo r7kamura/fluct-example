cd05b6219ddac217fda37628e6929f03bb531610
Ryo Nakamura <r7kamura@gmail.com>
2015-08-13 01:03:58 +0900

    Implement GET /readme

--- actions/show_readme/index.js
+++ actions/show_readme/index.js
@@ -1,3 +1,9 @@
+var marked = require('marked');
+var fs = require('fs');
+
 exports.handler = function (event, context) {
-  context.succeed('Hello, world!');
+  var markdownFilePath = __dirname + '/../../README.md';
+  var markdownString = fs.readFileSync(markdownFilePath, { encoding: 'utf8' });
+  var responseBody = marked(markdownString);
+  context.succeed(responseBody);
 };
