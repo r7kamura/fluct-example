10b065ee10de7843f41eca529eff1d57eaf6ff78
Ryo Nakamura <r7kamura@gmail.com>
2015-08-13 00:42:15 +0900

    Render HTML by jade

--- actions/show_root/index.js
+++ actions/show_root/index.js
@@ -1,3 +1,7 @@
+var jade = require('jade');
+
 exports.handler = function (event, context) {
-  context.succeed('Hello, world!');
+  var filePath = __dirname + '/views/index.jade';
+  var responseBody = jade.renderFile(filePath);
+  context.succeed(responseBody);
 };
